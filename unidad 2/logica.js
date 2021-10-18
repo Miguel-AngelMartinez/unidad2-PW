87ba4818814e9932d16abf63e4aea5ab260323a1
Miguel Angel Martinez <miguel_1319104805@uptecamac.edu.mx>
2021-10-18 15:26:11 -0500

    subido

---
+++ unidad 2/logica.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+     $("#validado").click(function () { 
+
+        var nombre=$("#uno").val();     
+        var mail=$("#dos").val();     
+        var numero=$("#tres").val();     
+        var password=$("#cuatro").val();     
+        var op=$("#cinco").val();     
+        
+        alert(`bienvenid@: ${nombre}`);
+
+        
+     });
+});
